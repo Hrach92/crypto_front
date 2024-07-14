3150677ad035b39fb67f98a5e654ad6a73272d54
Hrach92 <hrach.gh.92@mail.ru>
2024-07-14 14:04:59 +0400

    delete unused

--- src/components/loadable/index.tsx
+++ src/components/loadable/index.tsx
@@ -3,8 +3,6 @@ import { ElementType, Suspense } from "react";
 type PropTypes = object;
 
 const Loadable = (Component: ElementType) => (props: PropTypes) => {
-  console.log(props);
-
   return (
     <Suspense fallback={"loading..."}>
       <Component {...props} />
